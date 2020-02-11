49c5aedc7ff7a37a43d90f7c66c3df1bce58d6b6
Sergey Malinichev <malini4@MacBook-Air-Sergey.local>
2020-02-11 20:17:16 +0800

    first commit2

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/myApi',{ useNewUrlParser: true, useUnifiedTopology: true });
+const MONGO_USERNAME = 'serg';
+const MONGO_PASSWORD = '31415926535Ss';
+const MONGO_HOSTNAME = 'mongo';
+const MONGO_PORT = '27017';
+const MONGO_DB = 'myApi';
+
+// const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+
+mongoose.connect(`mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })   // указываем имя БД
+// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })   // указываем имя БД
+    .then(() => console.log('campDB Connected!'))
+    .catch(err => {
+    console.log(`DB Connection Error: ${err.message}`);
+    });
+
+// mongoose.connect('mongodb://mongo/myApi',{ useNewUrlParser: true, useUnifiedTopology: true });
 //export MODELS
 const Category = require('./model/category.js');
 const Item = require('./model/item.js');
@@ -16,11 +32,11 @@ seedDB();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//       next();
+//   });
 app.get('/api',(req,res)=>{
         //найти пользователя
     //найти все посты пользователя
@@ -60,7 +76,7 @@ app.post('/api/category/new', (req,res)=>{
     
             cat.save();
         
-            res.redirect('http://localhost:8080/')
+            res.redirect('/')
     }else{
         res.send('ничего не получилось')
     }
